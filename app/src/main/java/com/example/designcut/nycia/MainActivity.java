ac86a4f0e0f46a558b75c577ad248812b9df2796
engineersworkbook <shivamkalra2013@gmail.com>
2018-01-03 03:04:22 +0530

    Corrected fragments

--- app/src/main/java/com/example/designcut/nycia/MainActivity.java
+++ app/src/main/java/com/example/designcut/nycia/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.designcut.nycia;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.designcut.nycia.user.AccountFragment;
+import com.example.designcut.nycia.user.HomeFragment;
+import com.example.designcut.nycia.user.SearchFragment;
 
+public class MainActivity extends AppCompatActivity {
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -16,15 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager manager =getSupportFragmentManager();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
+                    manager.beginTransaction().replace(R.id.master,new AccountFragment()).commit();
                     return true;
-                case R.id.navigation_dashboard:
-
+                case R.id.navigation_search:
+                    manager.beginTransaction().replace(R.id.master,new SearchFragment()).commit();
                     return true;
-                case R.id.navigation_notifications:
-
+                case R.id.navigation_account:
+                    manager.beginTransaction().replace(R.id.master,new AccountFragment()).commit();
                     return true;
             }
             return false;
@@ -39,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+            FragmentManager manager= getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.master,new HomeFragment()).commit();
     }
 
 }
